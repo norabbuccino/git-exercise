785d54c014351b1e15e4da57a7d3ad78285945a8
norabbuccino <bresette@grinnell.edu>
2014-01-21 10:50:51 -0600

    Update HelloWorld.java

--- src/edu/grinnell/csc207/bresette/hello/HelloWorld.java
+++ src/edu/grinnell/csc207/bresette/hello/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld
   public static void
     main (String[] args)
   {
-    System.out.println( "Hello, GitHub!");
+    System.out.println( "Hello there GitHub!");
 
   }// main(String[])
 
