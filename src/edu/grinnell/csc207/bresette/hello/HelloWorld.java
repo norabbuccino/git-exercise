334d71339d4e76cdcb41cd91209a541ff76b6aae
norabbuccino <bresette@grinnell.edu>
2014-01-21 10:49:13 -0600

    Adding HelloWorld

---
+++ src/edu/grinnell/csc207/bresette/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package edu.grinnell.csc207.bresette.hello;
+
+public class HelloWorld
+{
+
+  public static void
+    main (String[] args)
+  {
+    System.out.println( "Hello, GitHub!");
+
+  }// main(String[])
+
+}
